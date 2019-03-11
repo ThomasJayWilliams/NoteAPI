591c94909f1024af2c31d48e282058b4e828642e
unknown <thatwaseclipse@gmail.com>
2019-03-11 16:37:57 +0200

    Fixed minor bugs

--- NoteAPI/NoteAPI/Startup.cs
+++ NoteAPI/NoteAPI/Startup.cs
@@ -1,17 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-using NoteAPI.Services;
+using Microsoft.AspNetCore.StaticFiles;
 using NoteAPI.Models;
-using Microsoft.EntityFrameworkCore;
+using NoteAPI.Services;
 
 namespace NoteAPI
 {
@@ -31,10 +26,6 @@ namespace NoteAPI
 			services.AddDbContext<NoteAPIContext>(options => options.UseSqlServer(con));
 			services.AddSingleton<TokenService>();
 			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-			services.AddSwaggerGen(c =>
-			{
-				c.SwaggerDoc("v1", new Swashbuckle.AspNetCore.Swagger.Info() { Title = "NoteAPI", Version = "1.0" });
-			});
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,13 +34,9 @@ namespace NoteAPI
 			if (env.IsDevelopment())
 				app.UseDeveloperExceptionPage();
 
-			app.UseMvc();
+			app.UseStaticFiles();
 
-			app.UseSwagger();
-			app.UseSwaggerUI(c =>
-			{
-				c.SwaggerEndpoint("./swagger/v1/swagger.json", "NoteAPI");
-			});
+			app.UseMvc();
 		}
 	}
 }
